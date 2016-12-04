bbe134dde72a8fde54178b083a5d07ec299ab9f0
lajiasd123 <lajiasd123@126.com>
2016-12-04 18:28:58 +0800

    Add files via upload

---
+++ image.py
@@ -0,0 +1,16 @@
+import re
+import pyperclip
+import sys
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
+
+neirong = str(pyperclip.paste())
+tupian_tiaojian = re.compile(r'http:\/\/[^\s]+.jpg')
+tupian_url = tupian_tiaojian.findall(neirong)
+shuchu = []
+for i in tupian_url:
+    shuchu.append(i)
+print shuchu
+
+
+
